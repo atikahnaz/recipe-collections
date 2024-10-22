de18ec4d721aa00378e84e7951050bff88aab3e3
Atikah <atikahnazir6@gmail.com>
2024-10-22 18:03:00 +0800

    feat: delete button for card recipe

---
+++ src/components/DeleteRecipe.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRecipeListStore } from "../stores/recipeStore";
+
+const store = useRecipeListStore();
+const props = defineProps({
+  recipe: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+<template>
+  <button @click="() => store.deleteRecipe(props.recipe.name)">Delete</button>
+</template>
