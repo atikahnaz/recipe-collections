7f674006b3aebc4202244750a42db1d5cb4f2258
Atikah <atikahnazir6@gmail.com>
2024-10-24 10:37:22 +0800

    feat: search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import InputText from "primevue/inputtext";
+import { useRecipeListStore } from "../stores/recipeStore";
+import { ref } from "vue";
+import InputIcon from "primevue/inputicon";
+
+const store = useRecipeListStore();
+const searchText = ref<string>("");
+
+const handleSearch = () => {
+  store.searchRecipe(searchText.value); // Make sure to use searchText.value to get the string
+};
+</script>
+
+<template>
+  <div class="flex items-center gap-2 my-2">
+    <InputText
+      placeholder="Search Ingredients"
+      type="text"
+      class="w-40 sm:w-auto"
+      v-model="searchText"
+    />
+    <InputIcon>
+      <i class="pi pi-search mx-2" @click="handleSearch" />
+    </InputIcon>
+  </div>
+</template>
