1252e9ee6c3f72593a6e02049efeeac028fbef9f
Atikah <atikahnazir6@gmail.com>
2024-10-25 15:28:44 +0800

    style: change button color and text

--- src/components/DeleteRecipe.vue
+++ src/components/DeleteRecipe.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 <template>
   <button
     @click="() => store.deleteRecipe(props.recipe.name)"
-    class="bg-black text-white px-3 py-1 rounded-lg mr-2"
+    class="bg-black text-white px-4 py-1 rounded-lg mr-2 text-sm md:text-base"
   >
     Delete
   </button>
