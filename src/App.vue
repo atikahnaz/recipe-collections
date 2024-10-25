c7d010d7ee779c69c0278a4e6a8e63622279cc8d
Atikah <atikahnazir6@gmail.com>
2024-10-25 11:12:31 +0800

    style: add padding feat: bind recipe to recipecard component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+//import HelloWorld from "./components/HelloWorld.vue";
 import { useRecipeListStore } from "./stores/recipeStore";
 import "./style.css";
 import { onMounted, ref } from "vue";
 import RecipeListDataView from "./components/RecipeListDataView.vue";
+import RecipeCard from "./components/RecipeCard.vue";
+import SearchBar from "./components/SearchBar.vue";
 
-const store = useRecipeListStore();
+const store: any = useRecipeListStore();
 
 onMounted(() => {
   store.fetchdata();
@@ -13,6 +15,13 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>Recipe Collections</h1>
-  <RecipeListDataView />
+  <div class="px-6 lg:py-10 lg:px-14">
+    <h1 class="text-xl lg:text-2xl font-semibold py-3">My Recipe</h1>
+    <div class="mt-2 mb-8">
+      <RecipeCard :recipe="store.recipeLocallySaved" />
+    </div>
+    <h1 class="text-xl lg:text-2xl font-semibold py-3">Recipe Collections</h1>
+    <SearchBar />
+    <RecipeListDataView />
+  </div>
 </template>
