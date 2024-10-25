01fae65e22018851d06f5e287c970103549aeb33
Atikah <atikahnazir6@gmail.com>
2024-10-25 11:15:14 +0800

    feat: add function to display modalRecipe

--- src/components/RecipeListDataView.vue
+++ src/components/RecipeListDataView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref, computed } from "vue";
 //import { ProductService } from "@/service/ProductService";
 import { useRecipeListStore } from "../stores/recipeStore";
 import DataView from "primevue/dataview";
 import SaveButton from "./SaveButton.vue";
+import RecipeForm from "./RecipeForm.vue";
 
 const store = useRecipeListStore();
 
@@ -12,6 +13,14 @@ const store = useRecipeListStore();
 // });
 onMounted(() => {
   store.fetchdata();
+});
+
+const recipeForm = ref<Object>({});
+
+const displayedRecipes = computed(() => {
+  return store.searchRecipeResult.length > 0
+    ? store.searchRecipeResult
+    : store.recipeApi;
 });
 
 const imagedefault: string =
@@ -31,66 +40,77 @@ const imageErrorDisplay = (event: Event) => {
   const target = event.target as HTMLImageElement;
   target.src = imagedefault; // Set the image source to default if the image is broken
 };
+
+// modal recipe
+const modalVisible = ref<Boolean>(false);
+
+const openModalRecipe = (item: Object) => {
+  modalVisible.value = true;
+  recipeForm.value = item;
+  console.log(item);
+};
+
+const closeModalRecipe = () => {
+  modalVisible.value = false;
+};
 </script>
 
 <template>
+  <!-- <div>{{ recipeForm.name }}</div> -->
+  <RecipeForm
+    v-if="modalVisible"
+    :recipe="recipeForm"
+    :isSavedRecipe="false"
+    @close="closeModalRecipe"
+  ></RecipeForm>
   <div class="card">
-    <DataView :value="store.recipeApi" paginator :rows="5">
+    <p v-if="store.noRecipeMessage" class="py-3">{{ store.noRecipeMessage }}</p>
+
+    <DataView :value="displayedRecipes" paginator :rows="5">
       <template #list="slotProps">
         <div class="flex flex-col">
-          <div v-for="(item, index) in slotProps.items" :key="index">
+          <div
+            v-for="(item, index) in slotProps.items"
+            :key="index"
+            class="mb-6"
+          >
             <div
-              class="flex flex-col sm:flex-row sm:items-center p-6 gap-4"
+              class="flex flex-col md:flex-row sm:items-center gap-4"
               :class="{
                 'border-t border-surface-200 dark:border-surface-700':
                   index !== 0,
               }"
             >
-              <div class="md:w-40 relative">
+              <div
+                class="md:w-40 relative"
+                @click="openModalRecipe(item)"
+                @close="closeModalRecipe"
+              >
                 <img
                   class="block xl:block mx-auto rounded w-full"
                   :src="currentImage(item.image)"
                   :alt="item.name"
                   @error="imageErrorDisplay"
                 />
-                <div
-                  class="absolute bg-black/70 rounded-border"
-                  style="left: 4px; top: 4px"
-                >
-                  <p>status</p>
-                </div>
               </div>
               <div
-                class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6"
+                class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6 mb-4"
               >
                 <div
-                  class="flex flex-row md:flex-col justify-between items-start gap-2"
+                  class="flex flex-col justify-between items-start gap-2"
+                  @click="openModalRecipe(item)"
                 >
                   <div>
-                    <span
-                      class="font-medium text-surface-500 dark:text-surface-400 text-sm"
-                      >category</span
-                    >
                     <div class="text-lg font-medium mt-2">{{ item.name }}</div>
                   </div>
                   <div class="bg-surface-100 p-1" style="border-radius: 30px">
-                    <div
-                      class="bg-surface-0 flex items-center gap-2 justify-center py-1 px-2"
-                      style="
-                        border-radius: 30px;
-                        box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04),
-                          0px 1px 2px 0px rgba(0, 0, 0, 0.06);
-                      "
-                    >
-                      <span class="text-surface-900 font-medium text-sm"
-                        >rating</span
-                      >
-                      <i class="pi pi-star-fill text-yellow-500"></i>
-                    </div>
+                    <p class="line-clamp-3 text-sm overflow-y-scroll">
+                      {{ item.description }}
+                    </p>
                   </div>
                 </div>
-                <div class="flex flex-col md:items-end gap-8">
-                  <p>price</p>
+                <div class="flex flex-col items-end gap-8">
+                  <!-- <p>price</p> -->
                   <SaveButton :recipe="item"></SaveButton>
                 </div>
               </div>
