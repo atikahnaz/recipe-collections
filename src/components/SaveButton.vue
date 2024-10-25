0a925e4a27bdddef2ea2258cfb07dcbf369fb4e7
Atikah <atikahnazir6@gmail.com>
2024-10-25 11:11:31 +0800

    feat: change save button to saved when data saved

--- src/components/SaveButton.vue
+++ src/components/SaveButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useRecipeListStore } from "../stores/recipeStore";
-import { defineProps } from "vue";
+import { defineProps, computed } from "vue";
 
 const store = useRecipeListStore();
 const props = defineProps({
@@ -9,11 +9,25 @@ const props = defineProps({
     required: true,
   },
 });
+
+const isRecipeSaved = computed<boolean>(() =>
+  store.recipeLocallySaved.some(
+    (item: { name: string }) => item.name === props.recipe.name
+  )
+);
 </script>
 <template>
   <button
+    v-if="isRecipeSaved"
+    class="bg-white text-black px-3 py-1 rounded-lg mr-2 border shadow-sm"
+  >
+    Saved
+  </button>
+
+  <button
+    v-else
     @click="() => store.saveRecipeToLocalStorage(props.recipe)"
-    class="bg-black text-white px-3 py-1 rounded-lg mr-2"
+    class="bg-black text-white px-4 py-1 rounded-lg mr-2"
   >
     Save
   </button>
