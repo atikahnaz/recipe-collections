f4571302b0568830bfd090a6966398025caab996
Atikah <atikahnazir6@gmail.com>
2024-10-25 15:28:13 +0800

    fixed: removed console.log

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import Carousel from "primevue/carousel";
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import DeleteRecipe from "./DeleteRecipe.vue";
 import RecipeForm from "./RecipeForm.vue";
 
-//const products = ref<Object>({});
 const props = defineProps({
   recipe: {
     type: Object,
@@ -13,17 +12,11 @@ const props = defineProps({
 });
 
 const recipeForm = ref<Object>({});
-// const displayedSavedRecipes = computed(() => {
-//   console.log(props.recipe);
-//   return props.recipe.length > 0 && props.recipe;
-// });
 
 const imagedefault: string =
   "https://primefaces.org/cdn/primevue/images/usercard.png";
 
 const currentImage = (image: string | string[] | undefined): string => {
-  //console.log(image);
-  //console.log(image[0]);
   return image && image.length > 0
     ? image[0]
     : typeof image === "string"
@@ -39,13 +32,12 @@ const imageErrorDisplay = (event: Event) => {
 // modal recipe
 const modalVisible = ref<Boolean>(false);
 
-const openModalRecipe = (item: Object) => {
+const openModalRecipe = (item: Object): void => {
   modalVisible.value = true;
   recipeForm.value = item;
-  console.log(item);
 };
 
-const closeModalRecipe = () => {
+const closeModalRecipe = (): void => {
   modalVisible.value = false;
 };
 
@@ -71,8 +63,6 @@ const responsiveOptions = ref([
     numScroll: 1,
   },
 ]);
-
-console.log("card");
 </script>
 
 <template>
@@ -92,7 +82,7 @@ console.log("card");
     >
       <template #item="slotProps">
         <div
-          class="border-surface-200 dark:border-surface-700 rounded m-2 h-72 md:h-96"
+          class="border-surface-200 dark:border-surface-700 rounded m-4 h-80 md:h-96"
         >
           <div class="mb-4 h-2/3">
             <div
@@ -104,21 +94,16 @@ console.log("card");
                 :src="currentImage(slotProps.data.image)"
                 :alt="slotProps.data.name"
                 @error="imageErrorDisplay"
-                class="w-full h-full object-contain rounded"
+                class="w-full h-full object-cover rounded"
               />
-              <!-- <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" class="absolute" style="left:5px; top: 5px"/> -->
             </div>
           </div>
 
-          <div class="flex flex-col justify-between items-center">
-            <div class="mb-4 font-medium">{{ slotProps.data.name }}</div>
+          <div class="flex flex-col justify-between items-center h-1/3">
+            <div class="font-medium text-xs md:text-base">
+              {{ slotProps.data.name }}
+            </div>
             <DeleteRecipe :recipe="slotProps.data" />
-            <!-- <div class="mt-0 font-semibold text-xl">${{ slotProps.data.price }}</div> -->
-            <!-- <span>
-              <Button icon="pi pi-heart" severity="secondary" outlined />
-              <Button icon="pi pi-shopping-cart" class="ml-2" />
-              
-            </span> -->
           </div>
         </div>
       </template>
