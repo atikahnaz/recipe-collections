76d46f376b9dbf4c07b0c35fe4f3f9bfcc6d727a
Atikah <atikahnazir6@gmail.com>
2024-10-26 16:01:59 +0800

    feat: add hero section component

---
+++ src/components/HeroImage.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import heroImage from "../assets/pexels-ella-olsson-572949-1640770.jpg";
+import SearchBar from "./SearchBar.vue";
+</script>
+<template>
+  <div class="relative">
+    <div class="border border-red-700 md:h-96">
+      <img
+        :src="heroImage"
+        alt="recipe-search"
+        class="h-full w-full object-cover"
+      />
+    </div>
+    <div class="absolute flex flex-col items-center justify-center inset-0">
+      <div
+        class="bg-white bg-opacity-60 flex flex-col w-fit justify-center items-center py-6"
+      >
+        <h2
+          class="text-lg md:text-3xl font-semibold tracking-wide w-2/3 text-gray-800 text-left"
+        >
+          Discover and save recipes you'll love
+        </h2>
+        <div class="w-2/3 pt-2">
+          <SearchBar />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
