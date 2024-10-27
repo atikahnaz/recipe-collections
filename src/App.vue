b17962d99dbc689977342f6a27c1a7ae8e258cbc
Atikah <atikahnazir6@gmail.com>
2024-10-27 17:00:13 +0800

    feat: insert navigation bar and savedRecipepage

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import RecipeListDataView from "./components/RecipeListDataView.vue";
 import RecipeCard from "./components/RecipeCard.vue";
 import SearchBar from "./components/SearchBar.vue";
 import HeroImage from "./components/HeroImage.vue";
+import NavigationBar from "./components/NavigationBar.vue";
+import SavedRecipePage from "./components/SavedRecipePage.vue";
 
 const store: any = useRecipeListStore();
 
@@ -20,6 +22,10 @@ onMounted(() => {
     <HeroImage class="w-full" />
     <SearchBar class="absolute flex inset-0 justify-end" />
   </div> -->
+  <div class="md:px-14 px-8">
+    <NavigationBar />
+  </div>
+
   <HeroImage />
   <div class="px-6 lg:py-10 lg:px-14">
     <h1 class="text-xl lg:text-2xl font-semibold py-3">My Recipe</h1>
@@ -29,5 +35,11 @@ onMounted(() => {
     <h1 class="text-xl lg:text-2xl font-semibold py-3">Recipe Collections</h1>
     <SearchBar />
     <RecipeListDataView />
+  </div>
+  <div
+    v-if="store.openSavedPage"
+    class="fixed overflow-auto top-0 px-4 py-6 w-full h-full bg-white"
+  >
+    <SavedRecipePage />
   </div>
 </template>
