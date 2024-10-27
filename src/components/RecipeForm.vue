2665e7dc1f65a9aa4854fe9e9b37b37a726075d9
Atikah <atikahnazir6@gmail.com>
2024-10-27 16:44:33 +0800

    style: change image size, title position

--- src/components/RecipeForm.vue
+++ src/components/RecipeForm.vue
@@ -34,6 +34,14 @@ const imageErrorDisplay = (event: Event): void => {
   }
 };
 
+const descriptionCleaned = (description: string) => {
+  if (description) {
+    return description.replace(/&nbsp;/g, " ").replace(/\u2014/g, "—");
+  } else {
+    return;
+  }
+};
+
 const emit = defineEmits(["close"]);
 </script>
 
@@ -42,6 +50,20 @@ const emit = defineEmits(["close"]);
     class="bg-white dark:bg-gray-800 text-black dark:text-white z-40 fixed top-0 w-full h-full overflow-y-scroll left-0"
   >
     <div class="p-5 md:w-1/2 m-auto md:border md:my-4">
+      <div class="flex justify-between">
+        <div class="font-semibold text-lg">{{ props.recipe.name }}</div>
+        <button @click="emit('close')" class="self-start rounded-full pointer">
+          <i class="pi pi-times"></i>
+        </button>
+      </div>
+      <div v-if="props.recipe.author" class="text-sm">
+        <p><span class="">By </span>{{ props.recipe.author.name }}</p>
+      </div>
+
+      <div class="py-2">
+        {{ descriptionCleaned(props.recipe.description) }}
+      </div>
+
       <div class="flex justify-between py-4">
         <img
           :src="currentImage(props.recipe.image)"
@@ -49,25 +71,11 @@ const emit = defineEmits(["close"]);
           width="250"
           preview
           @error="imageErrorDisplay"
+          class="w-full"
         />
-        <button
-          @click="emit('close')"
-          class="self-start rounded-full mr-2 pointer"
-        >
-          <i class="pi pi-times"></i>
-        </button>
       </div>
 
       <div class="space-y-3">
-        <h6 class="font-semibold">{{ props.recipe.name }}</h6>
-        <div v-if="props.recipe.author && props.recipe.author.length > 0">
-          <p>
-            <span class="font-medium">Author: </span
-            >{{ props.recipe.author.name }}
-          </p>
-        </div>
-        <div v-else>No author</div>
-
         <div>
           <h6 class="font-medium">Ingredients</h6>
           <div
@@ -98,7 +106,7 @@ const emit = defineEmits(["close"]);
             <div
               v-for="(instruction, index) in props.recipe.recipeInstructions"
             >
-              <p>{{ index }} - {{ instruction.text }}</p>
+              <p>{{ index + 1 }} - {{ instruction.text }}</p>
             </div>
           </div>
           <div v-else-if="props.recipe.recipeInstructions">
