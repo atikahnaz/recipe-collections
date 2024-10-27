29eaf6c0ff35ffb8806020b64384751bc72295cc
Atikah <atikahnazir6@gmail.com>
2024-10-27 16:43:04 +0800

    feat: create button myrecipe and import card component for each recipe

---
+++ src/components/SavedRecipePage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import CardSingle from "./CardSingle.vue";
+import { useRecipeListStore } from "../stores/recipeStore";
+
+const store = useRecipeListStore();
+</script>
+
+<template>
+  <div class="bg-white md:px-8 lg:px-10 py-4">
+    <h1 class="text-xl font-semibold">My Recipe</h1>
+    <button class="my-2 hover:cursor-pointer" @click="store.handleSavedPage">
+      Back
+    </button>
+    <div class="flex flex-wrap h-full justify-between">
+      <div v-for="recipe in store.recipeLocallySaved" class="flex">
+        <CardSingle :recipe="recipe" />
+      </div>
+    </div>
+  </div>
+</template>
