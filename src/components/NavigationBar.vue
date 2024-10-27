0c49c3861648597f5c732bd93cbddf4cdc4ce36d
Atikah <atikahnazir6@gmail.com>
2024-10-27 16:46:01 +0800

    feat: navigation bar with button to savedrecipe page

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRecipeListStore } from "../stores/recipeStore";
+
+const store = useRecipeListStore();
+</script>
+
+<template>
+  <div class="flex py-4 md:py-8 justify-between items-center">
+    <div class="md:text-xl">CookBook Recipe</div>
+    <div
+      class="bg-red-600 font-semibold text-white px-4 py-2 rounded-lg hover:bg-white hover:text-black hover:border hover:shadow-sm hover:cursor-pointer"
+      @click="store.handleSavedPage"
+    >
+      My Recipe
+    </div>
+  </div>
+</template>
