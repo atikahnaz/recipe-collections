98c1b64eff6c2bc17b3a548328960ace6886e59b
Atikah <atikahnazir6@gmail.com>
2024-10-28 11:41:20 +0800

    style: used grid to display card recipe

--- src/components/SavedRecipePage.vue
+++ src/components/SavedRecipePage.vue
@@ -11,8 +11,13 @@ const store = useRecipeListStore();
     <button class="my-2 hover:cursor-pointer" @click="store.handleSavedPage">
       Back
     </button>
-    <div class="flex flex-wrap h-full gap-2 justify-center">
-      <div v-for="recipe in store.recipeLocallySaved" class="flex">
+    <div
+      class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 h-full gap-2 justify-center"
+    >
+      <div
+        v-for="recipe in store.recipeLocallySaved"
+        class="flex justify-center"
+      >
         <CardSingle :recipe="recipe" />
       </div>
     </div>
